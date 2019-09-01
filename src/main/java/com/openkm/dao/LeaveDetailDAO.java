d52e5d88689a902fe85bc5d931b338988b9db869
sanjayzen <sanjayzen@gmail.com>
2019-09-01 14:32:20 +0530

    Added status

--- src/main/java/com/openkm/dao/LeaveDetailDAO.java
+++ src/main/java/com/openkm/dao/LeaveDetailDAO.java
@@ -61,13 +61,33 @@ public class LeaveDetailDAO {
 			HibernateUtil.close(session);
 		}
 	}
+	
+	/**
+	 * Update activity
+	 */
+	public static void update(LeaveDetail leaveDetail) throws DatabaseException {
+		Session session = null;
+		Transaction tx = null;
+
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			session.update(leaveDetail);
+			HibernateUtil.commit(tx);
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
 
 	/**
 	 * Find by filter
 	 */
 	@SuppressWarnings("unchecked")
-	public static List<LeaveDetail> findByUserId(String userName) throws DatabaseException {
-		log.debug("findByUserId({})", userName);
+	public static List<LeaveDetail> findByUserName(String userName) throws DatabaseException {
+		log.debug("findByUserName({})", userName);
 		String qs = "from LeaveDetail a where a.userName=:userName";
 
 		//qs += "order by a.date";
@@ -77,6 +97,33 @@ public class LeaveDetailDAO {
 			session = HibernateUtil.getSessionFactory().openSession();
 			Query q = session.createQuery(qs);
 			q.setString("userName", userName);
+
+			List<LeaveDetail> ret = q.list();
+			log.debug("findByUserName: {}", ret);
+			return ret;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+	
+	/**
+	 * Find by filter
+	 */
+	@SuppressWarnings("unchecked")
+	public static List<LeaveDetail> findPendingLeave(String approverName) throws DatabaseException {
+		log.debug("findByUserId({})", approverName);
+		String qs = "from LeaveDetail a where a.approverName=:approverName and a.isApproved = 'I'";
+
+		//qs += "order by a.date";
+		Session session = null;
+
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Query q = session.createQuery(qs);
+			q.setString("approverName", approverName);
 
 			List<LeaveDetail> ret = q.list();
 			log.debug("findByUserId: {}", ret);
@@ -92,9 +139,9 @@ public class LeaveDetailDAO {
 	 * Find by filter
 	 */
 	@SuppressWarnings("unchecked")
-	public static List<LeaveDetail> findPendingLeave(String approverName) throws DatabaseException {
-		log.debug("findByUserId({})", approverName);
-		String qs = "from LeaveDetail a where a.approverName=:approverName";
+	public static LeaveDetail findByLeaveId(int leaveId) throws DatabaseException {
+		log.debug("findByLeaveId({})", leaveId);
+		String qs = "from LeaveDetail a where a.leaveId=:leaveId";
 
 		//qs += "order by a.date";
 		Session session = null;
@@ -102,10 +149,10 @@ public class LeaveDetailDAO {
 		try {
 			session = HibernateUtil.getSessionFactory().openSession();
 			Query q = session.createQuery(qs);
-			q.setString("approverName", approverName);
+			q.setInteger("leaveId", leaveId);
 
-			List<LeaveDetail> ret = q.list();
-			log.debug("findByUserId: {}", ret);
+			LeaveDetail ret = (LeaveDetail)q.setMaxResults(1).uniqueResult();
+			log.debug("findByLeaveId: {}", ret);
 			return ret;
 		} catch (HibernateException e) {
 			throw new DatabaseException(e.getMessage(), e);
